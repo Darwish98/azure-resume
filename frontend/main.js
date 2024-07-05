15564c23415b12dcd2e8dc67b5742c4bdb61e803
CapDarwish <mohammed.darouich@capgemini.com>
2024-07-05 11:43:10 +0200

    Created azure functions api

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
-const functionAPI = '';
+const functionAPI = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
