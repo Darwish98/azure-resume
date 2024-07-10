0c2664c17168b35144bc76aa01ff6e33453c5603
CapDarwish <mohammed.darouich@capgemini.com>
2024-07-10 08:13:36 +0200

    added api

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApiUrl ='';
+const functionApiUrl ='https://getresumecounterfunction.azurewebsites.net/api/GetResumeCounter?code=asOcJKlYbhRSzoL8CCl4_kmxZK_OLG3GSKXy1XIQ3JpKAzFuoowFEA%3D%3D';
 const localFunctionAPI = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localFunctionAPI).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json();
     }).then(response => {
         count = response.count;
